50ad59d6e44b11c4bd21525628b0fb2b309625e7
Rômulo Albuquerque <romulo.albuquerque@live.com>
2020-11-30 15:10:00 -0300

    Update index.php
    antes de instalar extensão intelephense php

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
-include_once 'includes/header.php'
+include_once 'includes/header.php';
 ?>
-<div class="row">
 
+<div class="row">
+<div class="col s12 m6 push-m3 red">
+    
+</div> 
 </div>
+
 <?php
-include_once 'includes/footer.php'
+include_once 'includes/footer.php';
 ?>
