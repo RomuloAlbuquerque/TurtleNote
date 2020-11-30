5cb030185418ca9eec17689cf7d44ae02da334e2
Rômulo Albuquerque <romulo.albuquerque@live.com>
2020-11-30 15:19:31 -0300

    Inclusão <thead> da tabela

--- index.php
+++ index.php
@@ -3,9 +3,22 @@ include_once 'includes/header.php';
 ?>
 
 <div class="row">
-<div class="col s12 m6 push-m3 red">
-    
-</div> 
+    <div class="col s12 m6 push-m3 red">
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Título</th>
+                    <th>Data</th>
+                    <th>Nota</th>
+                </tr>
+            </thead>
+
+            <tbody>
+            
+            </tbody>
+        </table>
+    </div>
 </div>
 
 <?php
